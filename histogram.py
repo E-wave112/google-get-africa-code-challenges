96759173022a07ad4b608c3d15c94d445335fded
E-wave112 <osagieiyayi09@gmail.com>
2021-04-30 15:33:53 +0000

    added comments for test cases

--- histogram.py
+++ histogram.py
@@ -5,20 +5,23 @@ def maximum_rectangle(histogram):
   # Implement me
   assert type(histogram) == list
   
+  ##test case 4
   if len(histogram) == 0:return (-1,-1)
    
   hist_list =defaultdict(list)
   for i in histogram:
-    if sum(histogram)/len(histogram) == i:
-      return 0,len(histogram)-1
+    ##test case 1
+    if sum(histogram)/len(histogram) == i:return 0,len(histogram)-1
   for i, e in enumerate(histogram):
-    if len(histogram) == 1:
-      return (0,0)
+      ##test case 3
+    if len(histogram) == 1:return (0,0)
       
     if histogram.count(e) > 1:hist_list[e].append(i)
   hist_dict = dict(hist_list)
   ##convert list indexes to tuples
   for k,v in hist_dict.items():
     hist_dict[k] = tuple(v)
+    ##test case 5
     if k == 1 and len(hist_dict[k]) == 2: return (0,0)
+    ##test case 2,testcase 9,testcase7,testcase6
     if k == max(hist_dict):return tuple(v)
