eeae38d58d7b7681cfa647eb87643fe623332c00
E-wave112 <osagieiyayi09@gmail.com>
2021-05-20 20:25:01 +0000

    added another new task

---
+++ car_plate_vocabulary.py
@@ -0,0 +1,26 @@
+
+def find_shortest_word(plate, vocabulary):
+    v = "".join([i for i in plate.strip() if i.isalpha()])
+    place_holder=''
+    ##convert to lowercase
+    v = v.lower()
+    vocab_lower = [i.lower() for i in vocabulary]
+    if v not in vocab_lower:
+        place_holder=""
+        
+    if len(vocab_lower)== 0:
+        place_holder=''
+        
+    ##create a vocabulary letter number mapping
+    vocab_keys=[chr(i) for i in range(97,123)]
+    vocab_values=[i for i in range(97,123)]
+    ##creating the vocabulary dictionary
+    vocab_map = dict(zip(vocab_keys,vocab_values))
+
+    # return "".join([i for i in vocab_lower if set(i)-set(v)==0])
+    for i in vocab_lower:
+        if len(set(i) - set(v)) == 0:
+            place_holder=i
+        elif len(set(i) - set(v)) == 1:
+            place_holder=i
+    return place_holder
