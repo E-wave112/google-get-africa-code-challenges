23589cd916ad399b475841857e5ce22bcc21d5ef
E-wave112 <osagieiyayi09@gmail.com>
2021-06-11 19:14:42 +0000

    edited test cases

--- car_plate_vocabulary.py
+++ car_plate_vocabulary.py
@@ -6,8 +6,7 @@ def find_shortest_word(plate, vocabulary):
     ##convert to lowercase
     v = v.lower()
     vocab_lower = [i.lower() for i in vocabulary]
-    if v not in vocab_lower:
-        place_holder=""
+    if v not in vocab_lower:return v
         
     if len(vocab_lower)== 0:
         place_holder=''
@@ -21,14 +20,14 @@ def find_shortest_word(plate, vocabulary):
     
     for i in vocab_lower:
         if len(set(i) - set(v)) == 0 or len(set(i) - set(v)) == 1:place_holder=i
-        if set(i) == set(v):return i
+        if set(i) == set(v):place_holder=i
     return place_holder
 
 
 
 class CarTestVocabulary(unittest.TestCase):
     def test_car_vocab(self):
-        self.assertEqual(find_shortest_word("RT 123 SO", ["car", "rest", "rust", "rot", "sir", "cast"]),'sort')
+        self.assertEquals(find_shortest_word("RT 123 SO", ["car", "rest", "rust", "rot", "sir", "cast"]),'sort')
         self.assertEqual(find_shortest_word("RC 10014", ["sort", "car", "rest", "rust", "sir", "cast"]),'car')
         self.assertEqual(find_shortest_word("Ps 3S1t0", ["step", "steps", "stripe", "stepple"]),'steps')
         self.assertEqual(find_shortest_word("ST 123 CA", ["cat", "act", "sat", "cast", "cast"]),'cast')
@@ -36,6 +35,3 @@ class CarTestVocabulary(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
