f13aac3b1e8994be33abb753b17ca6a62f8ca92d
E-wave112 <osagieiyayi09@gmail.com>
2021-07-31 18:12:00 +0000

    added a new challenge

---
+++ get-ahead-2/number_words.py
@@ -0,0 +1,12 @@
+# testing module
+import unittest
+
+
+singles = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine')
+
+doubles = ('Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen')
+
+tens_num = ('Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety', 'Hundred')
+
+suffixes_num = ('', 'Thousand', 'Million', 'Billion','Trillion','Quadrillion',
+'Quintillion','Sextillion','Septillion','Octillion','Nonillion','Decillion')
