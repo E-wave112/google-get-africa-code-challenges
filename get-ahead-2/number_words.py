80a41cc9c74c4fb6d25426b747396bc7c947ffd4
E-wave112 <osagieiyayi09@gmail.com>
2021-07-31 19:02:34 +0000

    added a new challenge

--- get-ahead-2/number_words.py
+++ get-ahead-2/number_words.py
@@ -9,4 +9,68 @@ doubles = ('Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixtee
 tens_num = ('Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety', 'Hundred')
 
 suffixes_num = ('', 'Thousand', 'Million', 'Billion','Trillion','Quadrillion',
-'Quintillion','Sextillion','Septillion','Octillion','Nonillion','Decillion')
+'Quintillion','Sextillion','Septillion','Octillion','Nonillion','Decillion',
+'Undecillion','Duodecillion','Tredecillion','Quattuordecilllion','Quindecillion',
+'Sexdecillion','Septendecillion','Octadecillion','Novemdecillion','Vingitillion')
+
+def process(number, index):
+    
+    if number=='0':
+        return 'Zero'
+    
+    length = len(number)
+    
+    if(length > 3):
+        return False
+    
+    number = number.zfill(3)
+    words = ''
+ 
+    hdigit = int(number[0])
+    tdigit = int(number[1])
+    odigit = int(number[2])
+    
+    words += '' if number[0] == '0' else singles[hdigit]
+    words += ' Hundred ' if not words == '' else ''
+    
+    if(tdigit > 1):
+        words += tens_num[tdigit - 2]
+        words += ' '
+        words += singles[odigit]
+    
+    elif(tdigit == 1):
+        words += doubles[(int(tdigit + odigit) % 10) - 1]
+        
+    elif(tdigit == 0):
+        words += singles[odigit]
+
+    if(words.endswith('Zero')):
+        words = words[:-len('Zero')]
+    else:
+        words += ' '
+     
+    if(not len(words) == 0):    
+        words += suffixes_num[index]
+        
+    return words;
+    
+def convert_to_words(num):
+    length = len(str(num))
+    
+    if length>20:
+        return 'This program supports upto 20 digit (64 bit) numbers.'
+    
+    count = length // 3 if length % 3 == 0 else length // 3 + 1
+    copy = count
+    words = []
+ 
+    for i in range(length - 1, -1, -3):
+        words.append(process(str(num)[0 if i - 2 < 0 else i - 2 : i + 1], copy - count))
+        count -= 1;
+
+    final_words = ''
+    for s in reversed(words):
+        temp = s + ' '
+        final_words += temp
+    
+    return final_words
