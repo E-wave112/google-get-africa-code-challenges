4a77f5335ff15fbce34bd651b5a8ced0080856fd
Osagie <osagieiyayi09@gmail.com>
2021-08-06 04:59:58 +0100

    added test cases

--- leetcode/README.md
+++ leetcode/README.md
@@ -8,3 +8,5 @@
 * A coding problem that adds arrays to integers and returns their integer sum. See problem statement [here](https://leetcode.com/problems/add-to-array-form-of-integer/)
 
 * A leetcode challenge problem that returns indices of  two numbers such that they add up to target. full problem statement [here](https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3836/)
+
+* A coding challenge to find the winner of the stone game. full problem description [here](https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3870/)
--- leetcode/stone_game.py
+++ leetcode/stone_game.py
@@ -1,5 +1,21 @@
+import  unittest
+from typing import List
+
+
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         alex_throws = [i for i in piles if piles.index(i) % 2 == 0]
         lee_throws = [i for i in piles if piles.index(i) % 2 != 0]
         return alex_throws != lee_throws
+
+soln = Solution()
+
+# create test cases
+class test_stone_game(unittest.TestCase):
+    def test_stone(self):
+        self.assertEqual(soln.stoneGame([5,3,4,5]),True)
+        self.assertEqual(soln.stoneGame([3,9,3,7,4,5]),True)
+        self.assertEqual(soln.stoneGame([6,3,4,6]),True)
+
+if __name__ == "__main__":
+    unittest.main()
