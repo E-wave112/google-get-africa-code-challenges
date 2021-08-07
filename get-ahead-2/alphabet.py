9873e43acd13ad4061d0e622a4d694f866a8b186
E-wave112 <osagieiyayi09@gmail.com>
2021-08-07 04:21:50 +0000

    added more test cases

--- get-ahead-2/alphabet.py
+++ get-ahead-2/alphabet.py
@@ -2,6 +2,7 @@
 import unittest
 
 def find_alphabet(words):
+    if len(words) == 0: return []
     # check if a letter of a word is in the alphabet
     # get a list of all letters in the alphabet both lower and upper case
     present_alphabets = []
@@ -9,19 +10,30 @@ def find_alphabet(words):
     lower_case_alphabet = [chr(i) for i in range(97, 123)]
     # merge the above lists
     alphabet = upper_case_alphabet + lower_case_alphabet
+    # sort the words list in alphabetical order
+    sorted_word = sorted(words)
+    sorted_words= [i for i in sorted_word]
     # check if each word contains a letter from the alphabet
-    for word in words:
+    for word in sorted_words:
         for letter in word:
             if letter in alphabet:
                 present_alphabets.append(letter)
-    # return the alphabet with the letters that are present in the words
-    return list(set(present_alphabets))
+
+    # convert to a set to get the unique alphabets and then back to a list and sort
+    present_unique_alphas = sorted(list(set(present_alphabets)))
+    # return the alphabet with the letters that are present in the words iteratively
+    return [i for i in present_unique_alphas]
 
 # write test cases
 class TestFindAlphabet(unittest.TestCase):
     # write a function to for five test cases
     def test_find_alphabet(self):
-        self.assertEqual(find_alphabet(["ART", "RAT", "CAT", "CAR"]),["A", "T", "R", "C"])
+        self.assertEqual(find_alphabet(["ART", "RAT", "CAT", "CAR"]),["A","C","R","T"])
+        self.assertEqual(find_alphabet(["a", "bbl", "bba" ]),["a", "b","l"])
+        self.assertEqual(find_alphabet(["zal", "zb", "zzlb","zzz"]),["a", "b","l","z"])
+        self.assertEqual(find_alphabet(["aBa", "aaa", "bB","bbB" ]),["B","a","b"])
+        self.assertEqual(find_alphabet([]),[])
+
 
 if __name__ == '__main__':
     unittest.main()
