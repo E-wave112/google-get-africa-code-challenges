f8110bc1242598389cefc5e2a64a1f73715a9da1
Osagie <osagieiyayi09@gmail.com>
2021-08-31 16:35:40 +0100

    added the last day leetcode challenge

--- leetcode/bst_two_sum.py
+++ leetcode/bst_two_sum.py
@@ -38,6 +38,6 @@ class BstSum(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()m
 
        
---
+++ leetcode/findmin_sort_rotated.py
@@ -0,0 +1,7 @@
+import unittest
+from typing import List
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
+        
